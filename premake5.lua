94d2db7f14e85353eee68f47d275c69e4f20fbb5
adopolis23 <weinhofer@usf.edu>
2025-05-22 21:29:03 -0400

    Initial Commit and Premake file

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+workspace "MyWorkspace"
+    configurations {"Debug", "Release"}
+    architecture "x86_64"
+    startproject "MyLibrary"
+
+project "MyLibrary"
+    kind "SharedLib" --tells premake to build a DLL
+    language "C++"
+    cppdialect "C++17"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "bin-int/%{cfg.buildcfg}" --intermediate directory
+
+    files { "src/**.h", "src/**.cpp" } 
+
+    filter "system:windows"
+        systemversion "latest"
+    
+    filter "configurations:Debug"
+        defines { "DEBUG" }  --this to be used for macros when defining __declspec(dllexport)
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
